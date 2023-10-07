92acc7656fdcf4d37bba932d1f192475da308a28
argunriza <argunrizaa@gmail.com>
2023-10-07 20:32:53 +0300

    Katmanlar Oluşturuldu.

---
+++ src/DotNetCoreMvcCms/Cms.Web.Mvc.Doctor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cms.Web.Mvc.Doctor.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ src/DotNetCoreMvcCms/Cms.Web.Mvc.Patient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cms.Web.Mvc.Patient.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
